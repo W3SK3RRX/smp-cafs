9cee704a07d556c33fb7da9cc6749b1497e03b6c
root <hianclaudio16@gmail.com>
2024-06-14 10:02:46 -0300

    v1.3

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('logout/', views.sair, name='sair'),
 
     #reset
-    path('usuarios/reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('usuarios/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('usuarios/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('usuarios/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    #path('usuarios/reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    #path('usuarios/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    #path('usuarios/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    #path('usuarios/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
